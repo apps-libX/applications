6162fd0f649b9436a0887cecfeefd19e57a18da3
anonymoussc <50c5ac69@opayq.com>
2015-12-13 03:47:58 +0700

    Updates migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->string('displayName')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('foursquare')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('github')->nullable();
+            $table->string('google')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('twitter')->nullable();
             $table->timestamps();
         });
     }
