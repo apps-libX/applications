133ab24ac940096a820125a4f45148917c884e1e
anonymoussc <50c5ac69@opayq.com>
2015-12-14 06:20:51 +0700

    Modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var elixir = require('laravel-elixir'),
-    build  = require('./gulp.build.json');
+    path   = require('./tasks/app.components.js');
 
 require('./tasks/angular.task.js');
 require('./tasks/bower.task.js');
@@ -19,20 +19,11 @@ require('laravel-elixir-livereload');
 elixir(function(mix) {
     'use strict';
 
-    if (build.published) {
-        build.base_directory = '';
-        build.application    = '';
-
-        for (var i = 0; i < build.components.length; i++) {
-            build.components[i] = '';
-        }
-    }
-
     mix
         .bower()
-        .angular(angularDirectory())
-        .sass(sassDirectory(), 'public/css')
-        .copy(copyDirectory(), 'public/views/')
+        .angular(path.angularDirectory())
+        .sass(path.sassDirectory(), 'public/css')
+        .copy(path.copyDirectory(), 'public/views/')
         .livereload([
             'public/js/vendor.js',
             'public/js/app.js',
@@ -44,65 +35,4 @@ elixir(function(mix) {
 });
 
 require('gulp').task('generate', require('./tasks/generators/tasks/generate.js').generate);
-
-function angularDirectory() {
-    'use strict';
-    var ngMod = ngModFn(),
-        ngDir = ngDirFn();
-
-    return ngMod.concat(ngDir);
-}
-
-function sassDirectory() {
-    'use strict';
-    return sassDirFn();
-}
-
-function copyDirectory() {
-    'use strict';
-    return cpDirFn();
-}
-
-function ngModFn() {
-    'use strict';
-    var n = [];
-    for (var x = 0; x < build.components.length; x++) {
-        n.push('./' + build.base_directory + build.components[x] + 'angular/*.module.js');
-    }
-
-    n.push('./' + build.base_directory + build.application + 'angular/*.module.js');
-
-    return n;
-}
-
-function ngDirFn() {
-    'use strict';
-    var m = ['./' + build.base_directory + build.application + 'angular/**/**/**/*.js'];
-    for (var y = 0; y < build.components.length; y++) {
-        m.push('./' + build.base_directory + build.components[y] + 'angular/**/**/**/*.js');
-    }
-
-    return m;
-}
-
-function sassDirFn() {
-    'use strict';
-    var m = ['./' + build.base_directory + build.application + 'angular/**/**/**/*.scss'];
-    for (var y = 0; y < build.components.length; y++) {
-        m.push('./' + build.base_directory + build.components[y] + 'angular/**/**/**/*.scss');
-    }
-
-    return m;
-}
-
-function cpDirFn() {
-    'use strict';
-
-    var c = ['./' + build.base_directory + build.application + 'angular/**/**/**/*.html'];
-    for (var d = 0; d < build.components.length; d++) {
-        c.push('./' + build.base_directory + build.components[d] + 'angular/**/**/**/*.html');
-    }
-
-    return c;
-}
 
---
+++ tasks/app.components.js
@@ -0,0 +1,95 @@
+/**
+ * Created by anonymous on 14/12/15 4:45.
+ */
+
+var appComponents = require('./app.components.json');
+
+var path = {
+    angularDirectory: angularDirectory,
+    sassDirectory   : sassDirectory,
+    copyDirectory   : copyDirectory
+};
+
+
+
+//////////////////////////////
+
+/*
+if (appComponents.published) {
+    appComponents.base_directory = '';
+    appComponents.application    = '';
+
+    for (var i = 0; i < appComponents.components.length; i++) {
+        appComponents.components[i] = '';
+    }
+}
+*/
+
+function angularDirectory() {
+    'use strict';
+
+    var ngMod = ngModFn(),
+        ngDir = ngDirFn();
+
+    return ngMod.concat(ngDir);
+}
+
+function sassDirectory() {
+    'use strict';
+
+    return sassDirFn();
+}
+
+function copyDirectory() {
+    'use strict';
+
+    return cpDirFn();
+}
+
+function ngModFn() {
+    'use strict';
+
+    var m = [];
+    for (var y = 0; y < appComponents.components.length; y++) {
+        m.push('./' + appComponents.base_directory + appComponents.components[y] + 'angular/' + appComponents.components[y].split('/')[1] + '.module.js');
+    }
+
+    m.push('./' + appComponents.base_directory + appComponents.application + 'angular/' + appComponents.application.split('/')[1] + '.module.js');
+
+    return m;
+}
+
+function ngDirFn() {
+    'use strict';
+
+    var m = ['./' + appComponents.base_directory + appComponents.application + 'angular/**/**/**/*.js'];
+    for (var y = 0; y < appComponents.components.length; y++) {
+        m.push('./' + appComponents.base_directory + appComponents.components[y] + 'angular/**/**/**/*.js');
+    }
+
+    return m;
+}
+
+function sassDirFn() {
+    'use strict';
+
+    var m = ['./' + appComponents.base_directory + appComponents.application + 'angular/**/**/**/*.scss'];
+    for (var y = 0; y < appComponents.components.length; y++) {
+        m.push('./' + appComponents.base_directory + appComponents.components[y] + 'angular/**/**/**/*.scss');
+    }
+
+    return m;
+}
+
+function cpDirFn() {
+    'use strict';
+
+    var m = ['./' + appComponents.base_directory + appComponents.application + 'angular/**/**/**/*.html'];
+    for (var y = 0; y < appComponents.components.length; y++) {
+        m.push('./' + appComponents.base_directory + appComponents.components[y] + 'angular/**/**/**/*.html');
+    }
+
+    return m;
+}
+
+module.exports = path;
