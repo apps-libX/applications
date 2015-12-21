766dd5309d1c9bdb8f18c841a7e7489b46b113b1
anonymoussc <50c5ac69@opayq.com>
2015-12-21 11:10:15 +0700

    Update composer @ remove debugbar

--- config/app.php
+++ config/app.php
@@ -145,9 +145,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         Anwendungen\Applications\ApplicationsServiceProvider::class,
-
     ],
     /*
     |--------------------------------------------------------------------------
@@ -195,8 +193,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
-
     ],
 
 ];
